b8a7fa76da026107e3d5780b160de30f51b37ab5
Eddy <edkosyan2000@gmail.com>
2024-06-22 01:26:51 +0400

    change router

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/main.css";
 import { GlobalStateProvider } from "@reactivers/use-global-state";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pet_front">
+    <HashRouter  basename="/pet_front">
       <GlobalStateProvider>
         <App />
       </GlobalStateProvider>
-    </BrowserRouter>
+    </HashRouter >
   </React.StrictMode>
 );

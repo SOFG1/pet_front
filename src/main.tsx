af7a872e1a1f005e2ecb9f610aa50ed0154b4ccc
Eddy <edkosyan2000@gmail.com>
2024-06-22 01:30:06 +0400

    fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { HashRouter  } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter  basename="/pet_front">
+    <HashRouter>
       <GlobalStateProvider>
         <App />
       </GlobalStateProvider>

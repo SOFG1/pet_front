e1130913ca14ea0ae8df12d41f8c45de3f301264
Eddy <edkosyan2000@gmail.com>
2024-06-25 11:06:50 +0400

    add user bio feature

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      checkAuth(token);
+      checkAuth(token); 
     }
   }, []);          
    
